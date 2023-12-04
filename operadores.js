db1faffa33c2d389f14126a7e3e50e12b1f34d6c
omar huaman <neira.cn12@gmail.com>
2023-12-03 19:45:08 -0500

    mi segunda clase

---
+++ operadores.js
@@ -0,0 +1,18 @@
+// operador suma recuerda siempre guardar los cambios en (file) ocion (save)
+console.log(3 +5);
+// operador resta 
+console.log( 14 -9 );
+// operador multiplicacion 
+console.log(5*6);
+//operador divicion
+console.log(30/4);
+// modulo o residuo de una divicion %
+console.log(110%20);
+// potenciacion 
+console.log(4**2);
+// adicion de String 
+// recuerda comillas simples alt + 39
+console.log( ' hola ' + ' camilo ' )
+var nombre = ( 'camilo' );
+var saludo= " como estas " +nombre;
+console.log(saludo);
